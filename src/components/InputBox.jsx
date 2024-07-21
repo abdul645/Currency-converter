71226b10595ca320316170c3e6221238c0bca012
Abdul Azeem <abdulazeem3802@gmail.com>
2024-07-21 19:09:58 +0530

    changes

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -38,7 +38,7 @@ function InputBox({
             </div>
 
             <div className="w-1/2 flex flex-wrap justify-end text-right">
-                <p className="text-black/40 mb-2 w-full">Currency Type</p>
+                <p className="text-black/40 mb-2 w-full">Type</p>
                 <select 
                     className="rounded-lg px-1 w-10 py-1 bg-gray-100 cursor-pointer outline-none"
                     value={selectCurrency}
